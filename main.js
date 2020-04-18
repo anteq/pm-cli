04f4b3b63bf8ea55861d40a608cbae0167ce0b06
Antek Grzanka <antek@scalaric.com>
2020-04-18 19:01:11 +0200

    basic tray behaviour

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, globalShortcut, Menu, Tray } = require('electron')
 const path = require('path');
 
 function createWindow() {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 800,
@@ -12,18 +11,39 @@ function createWindow() {
         preload: path.join(__dirname, 'src/preload.js')
     }
   })
-
-  // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
+  mainWindow.on('blur', () => {
+    mainWindow.close();
+  });
   // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+app.on('ready', () => {
+  createTray();
+  registerShortcut();
+})
+
+function createTray() {
+  tray = new Tray('jira-spotlightTemplate.png')
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Open', type: 'normal', click: createWindow },
+    { label: 'Settings', type: 'normal' },
+    { type: 'separator' },
+    { label: 'Quit', type: 'normal', click: () => app.quit() },
+  ]);
+  tray.setToolTip('This is my application.');
+  tray.setContextMenu(contextMenu);
+}
+
+function registerShortcut() {
+  const ret = globalShortcut.register('Control+Command+Space', () => {
+    createWindow()
+  });
+  if (!ret) {
+    console.log('registration failed')
+  }
+}
+
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -32,11 +52,19 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+app.on('will-quit', () => {
+  globalShortcut.unregister('Control+Command+Space')
+  globalShortcut.unregisterAll()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+// app.whenReady().then(createWindow)
+
+// app.on('activate', function () {
+//   // On macOS it's common to re-create a window in the app when the
+//   // dock icon is clicked and there are no other windows open.
+//   if (BrowserWindow.getAllWindows().length === 0) createWindow()
+// })
