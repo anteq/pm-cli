41d263d35360600100cf4263865f9da521e69dd5
Antek Grzanka <antek@scalaric.com>
2020-04-23 21:38:15 +0200

    css refactor

--- src/preload.js
+++ src/preload.js
@@ -1,28 +1,51 @@
 const { remote, shell } = require('electron');
 const { parseInput } = require('./parser');
+const { api } = require('./config');
+const request = require('request');
 
 function main() {
   var result;
   window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('spotlight_input').focus();
+    document.querySelector('.input-search').focus();
     if (remote.getGlobal('darkMode')) {
-      document.querySelector('body').classList.add('dark');
+      document.querySelector('.content').classList.add('dark');
     }
   });
   window.addEventListener('keyup', (e) => {
-    result = parseInput(document.getElementById('spotlight_input').value);
+    result = parseInput(document.querySelector('.input-search').value);
     if (result) {
-      document.getElementById('spotlight_input').classList.add('match-found');
+      document.querySelector('.content').classList.remove('hide-main');
     } else {
-      document.getElementById('spotlight_input').classList.remove('match-found');
+      document.querySelector('.content').classList.add('hide-main');
     }
-    document.getElementById('spotlight_description').innerHTML = result ? result.text : '';
-    document.getElementById('spotlight_icon').innerHTML = result ? result.icon : '';
+    document.querySelector('.big-content__description').innerHTML = result ? result.text : '';
+    document.querySelector('.big-content__icon').innerHTML = result ? result.icon : '';
   });
   window.addEventListener('keydown', (e) => {
     if (e.key === 'Enter' && result && result.url) shell.openExternal(result.url, { activate: true });
     if (e.keyCode == '27') remote.getCurrentWindow().close();
   });
+
+  const options = {
+    url: 'https://scalaric.atlassian.net/rest/api/2/issue/TM-11132',
+    json: true,
+    headers: {
+      'Authorization': 'Basic ' + Buffer.from(api.jira.login + ":" + api.jira.key).toString("base64")
+    }
+  };
+
+  request.get(options, (err, res, data) => {
+    console.debug(res)
+    if (err) {
+      console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+      console.log('Status:', res.statusCode);
+    } else {
+      // data is already parsed as JSON:
+      console.log(data);
+    }
+});
+
 }
 
 main();
