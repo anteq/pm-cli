b0e17d30f7f0b7ac3cfd75ded78917bb5e8eeef3
Antek Grzanka <antek@scalaric.com>
2020-04-23 22:46:03 +0200

    remove req

--- src/preload.js
+++ src/preload.js
@@ -1,7 +1,5 @@
 const { remote, shell } = require('electron');
 const { parseInput } = require('./parser');
-const { api } = require('./config');
-const request = require('request');
 
 function main() {
   var result;
@@ -25,27 +23,6 @@ function main() {
     if (e.key === 'Enter' && result && result.url) shell.openExternal(result.url, { activate: true });
     if (e.keyCode == '27') remote.getCurrentWindow().close();
   });
-
-  const options = {
-    url: 'https://scalaric.atlassian.net/rest/api/2/issue/TM-11132',
-    json: true,
-    headers: {
-      'Authorization': 'Basic ' + Buffer.from(api.jira.login + ":" + api.jira.key).toString("base64")
-    }
-  };
-
-  request.get(options, (err, res, data) => {
-    console.debug(res)
-    if (err) {
-      console.log('Error:', err);
-    } else if (res.statusCode !== 200) {
-      console.log('Status:', res.statusCode);
-    } else {
-      // data is already parsed as JSON:
-      console.log(data);
-    }
-});
-
-}
+};
 
 main();
